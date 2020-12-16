ed1f87384d73dfc767b97fe0d4743ecb6d0c407e
SangDoan <doansang654@gmail.com>
2020-12-16 17:03:34 +0700

    check win

--- src/components/gamepage.js
+++ src/components/gamepage.js
@@ -7,14 +7,185 @@ import { socket } from "../App";
 import Chat from "../commons/Chat";
 import "../assets/custom.css";
 import { useParams } from "react-router-dom";
+import Square from "../commons/Square";
+import { constants } from "crypto";
 // import "../assets/login.css";
+const row = 20;
+const col = 30;
+function listToMatrix(list, elementsPerSubArray) {
+  var matrix = [],
+    i,
+    k;
+
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % elementsPerSubArray === 0) {
+      k++;
+      matrix[k] = [];
+    }
+
+    matrix[k].push(list[i]);
+  }
+
+  return matrix;
+}
+const checkWin = (square, just) => {
+  const _square = listToMatrix(square, col);
+  let cell = { x: Math.floor(just / col), y: just % col };
+  if (
+    isHorizontalCheck(_square, cell.x, cell.y) ||
+    isVerticalCheck(_square, cell.x, cell.y) ||
+    isPrimaryDiagCheck(_square, cell.x, cell.y) ||
+    isSubDiagCheck(_square, cell.x, cell.y)
+  ) {
+    return [true, square[just]];
+  }
+  return [false, square[just]];
+};
+function isHorizontalCheck(square, i, j) {
+  //int di = 0;
+  let dj = -1; // Di qua trai
+  //int startI = i;
+  let startJ = j;
+  //left
+  let countLeft = 1;
+  while (startJ + dj >= 0) {
+    startJ += dj; // Di qua trai
+    if (square[i][j] == square[i][startJ]) {
+      // Tang bien dem
+      countLeft++;
+    } else {
+      break;
+    }
+  }
+  //right
+  startJ = j;
+  dj = 1;
+  let countRight = 0;
+  while (startJ + dj <= col) {
+    startJ += dj; // Di qua phai
+    if (square[i][j] == square[i][startJ]) {
+      // Tang bien dem
+      countRight++;
+    } else {
+      break;
+    }
+  }
+
+  return countLeft + countRight == 5;
+}
+
+function isVerticalCheck(square, i, j) {
+  let di = -1;
+  let startI = i;
+  //top
+  let countTop = 1;
+  while (startI + di >= 0) {
+    startI += di; // Di len
+    if (square[i][j] == square[startI][j]) {
+      // Tang bien dem
+      countTop++;
+    } else {
+      break;
+    }
+  }
+  //right
+  startI = i;
+  di = 1;
+  let countDown = 0;
+  while (startI + di <= row) {
+    startI += di; // Di xuong
+    if (square[i][j] == square[startI][j]) {
+      // Tang bien dem
+      countDown++;
+    } else {
+      break;
+    }
+  }
+
+  return countTop + countDown == 5;
+}
+
+function isPrimaryDiagCheck(square, i, j) {
+  let di = -1;
+  let dj = -1;
+  let startI = i;
+  let startJ = j;
+  // .....
+  let countTop = 1;
+  while (startI + di >= 0 && startJ + dj >= 0) {
+    startI += di; // Di len
+    startJ += dj; // Đi qua trái
+    if (square[startI][startJ] == square[i][j]) {
+      // Tang bien dem
+      countTop++;
+    } else {
+      break;
+    }
+  }
+  //.....
+  startI = i;
+  di = 1;
+  startJ = j;
+  dj = 1;
+  let countDown = 0;
+  while (startI + di <= row && startJ + dj <= col) {
+    startI += di; // Di xuong
+    startJ += dj; // Đi qua phải
+    if (square[startI][startJ] == square[i][j]) {
+      // Tang bien dem
+      countDown++;
+    } else {
+      break;
+    }
+  }
+
+  return countTop + countDown == 5;
+}
+
+function isSubDiagCheck(square, i, j) {
+  let di = -1;
+  let dj = 1;
+  let startI = i;
+  let startJ = j;
+  // .....
+  let countTop = 1;
+  while (startI + di >= 0 && startJ + dj <= col) {
+    startI += di;
+    startJ += dj;
+    if (square[startI][startJ] == square[i][j]) {
+      // Tang bien dem
+      countTop++;
+    } else {
+      break;
+    }
+  }
+  //.....
+  startI = i;
+  di = 1;
+  startJ = j;
+  dj = -1;
+  let countDown = 0;
+  while (startI + di <= row && startJ + dj >= 0) {
+    startI += di;
+    startJ += dj;
+    if (square[startI][startJ] == square[i][j]) {
+      // Tang bien dem
+      countDown++;
+    } else {
+      break;
+    }
+  }
+
+  return countTop + countDown == 5;
+}
 export default function Gamepage() {
   const { id } = useParams();
   const user = AccountService.getCurrentUserInfo();
   const [isInit, setIsInit] = useState(false);
   const temp = ["X", "O"];
   const [q, setQ] = useState(0);
-  const [square, setSquare] = useState(Array(10 * 20).fill(null));
+  const [end, setEnd] = useState(false);
+  const [square, setSquare] = useState(Array(20 * 30).fill(null));
   const handleClick = (i) => {
     const newsquare = [...square];
     if (newsquare[i] == null) {
@@ -22,8 +193,13 @@ export default function Gamepage() {
     }
     setQ(1 - q);
     setSquare(newsquare);
+    let [res, turn] = checkWin(newsquare, i);
+    if (res) {
+      alert(`${turn} win`);
+      setEnd(res);
+    }
     console.log(newsquare);
-    let cell = { x: Math.floor(i / 20), y: i % 20 };
+    let cell = { x: Math.floor(i / col), y: i % col };
   };
   useEffect(() => {
     if (!isInit) {
@@ -77,8 +253,8 @@ export default function Gamepage() {
         <div className="game">
           <div className="game-board">
             <Board
-              row={10}
-              col={20}
+              row={row}
+              col={col}
               square={square}
               onClick={(i) => handleClick(i)}
             />
@@ -86,7 +262,10 @@ export default function Gamepage() {
         </div>
       </div>
       <div className="col-md-4">
-        <Chat></Chat>
+        <div className="game-board">
+          <h3>Message</h3>
+          <Chat></Chat>
+        </div>
       </div>
     </div>
   );
