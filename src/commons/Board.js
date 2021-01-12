2ec2c8b3388666c5a0c25d34b146ef03ab157297
Đức Thắng <thangvu8948@gmail.com>
2021-01-12 13:44:53 +0700

    + show win row

--- src/commons/Board.js
+++ src/commons/Board.js
@@ -5,14 +5,14 @@ export default function Board(props) {
   const createBoard = (row, col) => {
     const board = [];
     let cellCounter = 0;
-
+    console.log(props.winRow);
     for (let i = 0; i < row; i += 1) {
       const columns = [];
       for (let j = 0; j < col; j += 1) {
         columns.push(renderSquare(cellCounter++));
       }
       board.push(
-        <div key={i} className="board-row">
+        <div key={i} className='board-row'>
           {columns}
         </div>
       );
@@ -26,6 +26,7 @@ export default function Board(props) {
         key={i}
         value={props.square[i]}
         onClick={() => props.onClick(i)}
+        isWinCell={props.winRow && props.winRow.includes(i)}
       />
     );
   };
--- src/commons/Square.js
+++ src/commons/Square.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Square = (props) => (
-  <button className={`square square-${props.value}`} onClick={props.onClick}>
+  <button className={`square square-${props.value}`} style={{backgroundColor:`${props.isWinCell ? "yellow" : ""}`}} onClick={props.onClick}>
     {props.value}
   </button>
 );
